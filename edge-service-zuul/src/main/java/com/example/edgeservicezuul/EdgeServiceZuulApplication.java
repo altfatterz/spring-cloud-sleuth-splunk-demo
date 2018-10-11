6b035018149ef2a728dc153b9c4808970c5f1e6d
Zoltan Altfatter <altfatterz@gmail.com>
2018-10-11 14:04:11 +0200

    cleanup and working zuul filter

--- edge-service-zuul/src/main/java/com/example/edgeservicezuul/EdgeServiceZuulApplication.java
+++ edge-service-zuul/src/main/java/com/example/edgeservicezuul/EdgeServiceZuulApplication.java
@@ -3,12 +3,19 @@ package com.example.edgeservicezuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@EnableZuulProxy
 @SpringBootApplication
+@EnableZuulProxy
 public class EdgeServiceZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EdgeServiceZuulApplication.class, args);
+    }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
     }
 }
